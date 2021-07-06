08a0e138c0c0d5aaae73091c995800c067cea0de
amolu <amolujagare@gmail.com>
2021-07-06 16:09:16 +0530

    new programs

---
+++ test/src/test/Addition.java
@@ -0,0 +1,19 @@
+package test;
+
+class Addition
+{
+public static void main(String[] args)
+{
+	int a ;
+	int b;
+	int c ;
+
+	a=10;
+	b=21;
+
+	c=a+b;
+
+	System.out.println("Addition=" + c);
+
+}
+}
---
+++ test/src/test/AdditionDecimal.java
@@ -0,0 +1,19 @@
+package test;
+
+class AdditionDecimal
+{
+public static void main(String[] args)
+{
+	double a ;
+	double b;
+	double c ;
+
+	a=10.34;
+	b=21.45;
+
+	c=a+b;
+
+	System.out.println("Addition=" + c);
+
+}
+}
